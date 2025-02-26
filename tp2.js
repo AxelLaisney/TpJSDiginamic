74e78ffbd61baec4ac9793d5a031b4d3708da256
AxelLaisney <axel.laisney@hotmail.com>
2025-02-26 11:51:13 +0100

    Initial commit

---
+++ tp2.js
@@ -0,0 +1,25 @@
+let num1 = 1;
+let num2 = 3;
+
+console.log("num 1 = " + num1 + " num2 = " + num2);
+
+console.log("num1 + numb2 = " + (num1 + num2));
+console.log("num1 - numb2 = " + (num1 - num2));
+console.log("num1 * numb2 = " + (num1 * num2));
+console.log("num1 / numb2 = " + (num1 / num2));
+console.log("num1 % numb2 = " + (num1 % num2));
+
+console.log("num1 > numb2 = " + (num1 > num2));
+console.log("num1 < numb2 = " + (num1 < num2));
+console.log("num1 == numb2 = " + (num1 == num2));
+console.log("num1 != numb2 = " + (num1 != num2));
+
+let b1 = true
+let b2 = false
+
+console.log("b1 is true and b2 is false");
+
+console.log("b1 && b2 = " + (b1 && b2))
+console.log("b1 && b2 = " + (b1 || b2))
+console.log("!b1 =" + !b1)
+console.log("!b1 = " + !b1)
